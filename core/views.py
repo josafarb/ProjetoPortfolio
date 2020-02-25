d9b77de6f7682bd6f16fd3abe834b8455c7b80eb
josafarb <josafaareboucaas@gmail.com>
2020-02-25 11:29:51 -0300

    Configurando a exibição das habilidades

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from .validador import *
 # Create your views here.
 from django.views.generic import TemplateView
 from pessoa.models import Pessoa
+from habilidade.models import  Habilidade
 
 
 class IndexView(TemplateView):
@@ -11,6 +12,9 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['pessoa'] = Pessoa.objects.filter(id=1).first()
+        p = Pessoa.objects.first()
+        if(p is not None):
+            context['pessoa'] = p
+            context['habilidade'] =  Habilidade.objects.filter(pessoa = p.id, ativo = True)
         return context
 
--- habilidade/models.py
+++ habilidade/models.py
@@ -11,7 +11,7 @@ from stdimage import StdImageField
 class Habilidade(models.Model):
     nome = models.CharField('Nome', max_length=100)
     descricao = models.TextField('Descrição')
-    imagem = StdImageField('Foto', upload_to='pessoa', variations={'tumb': {'width': 42, 'height': 42, 'crop': True}})
+    imagem = StdImageField('Foto', upload_to='habilidade', variations={'tumb': {'width': 100, 'height': 100, 'crop': True}})
     ativo = models.BooleanField('Ativo', default=True)
     pessoa = ForeignKey(Pessoa, on_delete=models.PROTECT, blank=True, null=True)
 
