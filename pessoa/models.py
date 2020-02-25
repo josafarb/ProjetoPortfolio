9df2b799dd6024917e80847fd5306c18db66ea28
josafarb <josafaareboucaas@gmail.com>
2020-02-25 11:07:27 -0300

    modelando as habilidades

--- pessoa/models.py
+++ pessoa/models.py
@@ -23,6 +23,7 @@ class Pessoa(models.Model):
     biografia = models.TextField('Sobre mim ')
     foto = StdImageField('Foto', upload_to='pessoa', variations={'tumb': {'width': 524, 'height': 566, 'crop': True}})
 
+
     def __str__(self):
         return self.nome;
 
